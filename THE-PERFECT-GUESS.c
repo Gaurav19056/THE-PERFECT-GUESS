195c1addb115ed8031bf2b50ff71e5852c64004c
Gaurav Drall <drallgaurav90@gmail.com>
2025-07-28 22:29:55 +0530

    Update THE-PERFECT-GUESS.c
    Add a congratulations message after guessing the right number,

--- THE-PERFECT-GUESS.c
+++ THE-PERFECT-GUESS.c
@@ -12,6 +12,31 @@ int main() {
     int guessed_number;
     // Print the random number
     //printf("Random number between 1 and 100: %d\n", random_number);
+    void displayAmazingOutcome() {
+    printf("\n");
+    custom_sleep(200);
+    printf("**************************************************\n");
+    custom_sleep(200);
+    printf("* *\n");
+    custom_sleep(200);
+    printf("* _   _   _   _   _   _   _   _   _   _   _    *\n");
+    custom_sleep(200);
+    printf("* / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\   *\n");
+    custom_sleep(200);
+    printf("* ( C | O | N | G | R | A | T | S | ! | ! | ! )  *\n");
+    custom_sleep(200);
+    printf("* \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/   *\n");
+    custom_sleep(200);
+    printf("* *\n");
+    custom_sleep(200);
+    printf("* Y O U   G U E S S E D   I T !        *\n");
+    custom_sleep(200);
+    printf("* *\n");
+    custom_sleep(200);
+    printf("**************************************************\n");
+    custom_sleep(500);
+    printf("\n");
+}
     do
     {
         printf("guess the number");
@@ -21,7 +46,7 @@ int main() {
         }else if(guessed_number<random_number){
             printf("higher number please!!\n");
         }else{
-            printf("congrats!!\n");
+            displayAmazingOutcome();
         }
         no_of_guesses++;
     } while (guessed_number!=random_number);
